df717c58f9bd37e3bf9ebce82875bae2294b1eb7
“fatemagit config --global user.name “fatema <fatemamo7amed@gmail.com>
2019-04-09 23:12:19 +0200

    new function help to add new movie to list

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,46 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { JSDocTagName } from '@angular/compiler/src/output/output_ast';
 //import {Movies} from '../mock-movies';
-
+import { NgForm } from "@angular/forms";
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.css']
 })
 export class MoviesComponent implements OnInit {
-  movies = [
-    { id: 1,
-        moviName: 'Moon Light',
-        rate:7.8,
-        rateImg:"../assets/images/star.jpg",
-        dirname:"Barry Jenkins",
-       autrname:"Tarell Alvin McCraney",
-       myArray: ['Mahershala Ali','Shariff Earp','Duan Sanderson'],
-       moviImg: "../assets/images/cover.jpg"},
-       { id: 2,
-        moviName: 'Fighter',
-        rate:7.8,
-        rateImg:"../assets/images/star.jpg",
-        dirname:"Barry Jenkins",
-       autrname:"Tarell Alvin McCraney",
-       myArray: ['Mahershala Ali','Shariff Earp','Duan Sanderson'],
-       moviImg: "../assets/images/cover1.jpg"},
-       { id: 3,
-        moviName: 'Star Wars',
-        rate:7.8,
-        rateImg:"../assets/images/star.jpg",
-        dirname:"Barry Jenkins",
-       autrname:"Tarell Alvin McCraney",
-       myArray: ['Mahershala Ali','Shariff Earp','Duan Sanderson'],
-       moviImg: "../assets/images/cover2.jpg"},
-       { id: 4,
-        moviName: 'Baby Driver',
-        rate:7.8,
-        rateImg:"../assets/images/star.jpg",
-        dirname:"Barry Jenkins",
-       autrname:"Tarell Alvin McCraney",
-       myArray: ['Mahershala Ali','Shariff Earp','Duan Sanderson'],
-       moviImg: "../assets/images/cover3.jpg"}
-];
+  movies = [];
+  
+  moviName='';
+  rate='';
+  rateImg='';
+  dirname='';
+  autrname='';
+  moviImg='';
+  myArray=[];
   selectedMovie;
   deleteMovi;
   constructor() { }
@@ -55,4 +31,18 @@ export class MoviesComponent implements OnInit {
     this.deleteMovi=movie;
     this.movies.splice(index, 1);
 }
+public getDataForm(formData:  NgForm) {
+  this.movies.push({
+    moviName:formData.value.moviName,
+    rate:formData.value.rate,
+    dirname:formData.value.dirname,
+    autrname:formData.value.autrname,
+    rateImg:'../../assets/images/star.jpg',
+  moviImg:'../../assets/images/cover.jpg',
+  myArray:['Mahershala Ali','Shariff Earp','Duan Sanderson']
+  });
+}
+// onAdd(){
+
+// }
 }
